d18c4d80f0be771c8b93475d089b80f27acd94bf
Mick Redman <guitarbeerchocolate@googlemail.com>
2013-07-02 14:08:50 +0100

    New version of database class includes mysqli.

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 require_once 'classes/autoload.php';
 $db = new database;
 
-/* Multiple rows
-$results = $db->query("SELECT * FROM `users`");
-foreach($results as $row)
-{
-	echo $row->id.'<br />';
-} */
+/* Multiple rows */
+// $results = $db->query("SELECT * FROM `users`");
+// foreach($results as $row)
+// {
+// 	echo $row->id.'<br />';
+// }
 
-/* Single row 
-$result = $db->singleRow("SELECT * FROM `users` WHERE `id`='2'");
+ // Single row
+$result = $db->singleRow("SELECT * FROM `users` WHERE `id`='4'");
 echo $result->username;
-*/
 ?>
